444ebb0f3cb9a8e16dc4986b85368415d01cb2c7
Andrei <soprachev@mail.ru>
2024-01-21 04:31:03 +0300

    fix migration

--- src/db/migration/2.ts
+++ src/db/migration/2.ts
@@ -4,10 +4,11 @@ export default {
   name: "2",
   up: `
   create view if not exists description as
-    select name, type, comment
-    from system.columns
-    where database = 'WOT'
-      and table != 'migrations';
+  select table, name, type, comment
+  from system.columns
+  where database = 'WOT'
+    and table != 'migrations'
+    and table != 'description';
   `,
   down: `
   drop view if exists description;
