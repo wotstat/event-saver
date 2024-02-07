9b48b932e8f9b460c821119a0eb8eea18691f8ec
Andrei <soprachev@mail.ru>
2024-02-07 19:06:49 +0300

    change migration 6

--- src/db/migration/6.ts
+++ src/db/migration/6.ts
@@ -3,7 +3,7 @@ import type { MigrationData } from "../migration";
 
 function getQuert(table: string) {
   return `
-  alter table ${table} add column modVersion_parts Array(String) materialized extractAllGroups(modVersion, '^(\d+)\.(\d+)\.(\d+)(?:\.(\d+))?(?:-([0-9A-Za-z-.]+))?$')[1] comment 'Части версии мода major.minor.patch.revision-identifier';
+  alter table ${table} add column if not exists modVersion_parts Array(String) materialized extractAllGroups(modVersion, '^(\d+)\.(\d+)\.(\d+)(?:\.(\d+))?(?:-([0-9A-Za-z-.]+))?$')[1] comment 'Части версии мода major.minor.patch.revision-identifier';
   alter table ${table} add column if not exists modVersion_major UInt16 materialized toUInt16(modVersion_parts[1])    comment 'Часть версии мода MAJOR.minor.patch.revision-identifier';
   alter table ${table} add column if not exists modVersion_minor UInt16 materialized toUInt16(modVersion_parts[2])    comment 'Часть версии мода major.MINOR.patch.revision-identifier';
   alter table ${table} add column if not exists modVersion_patch UInt16 materialized toUInt16(modVersion_parts[3])    comment 'Часть версии мода major.minor.PATCH.revision-identifier';
