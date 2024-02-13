6bc3687f862fa1f9c71e7d626fdcfa9bad03980f
Andrei <soprachev@mail.ru>
2024-02-14 02:23:50 +0300

    disable keep alive

--- src/db/rollback.ts
+++ src/db/rollback.ts
@@ -1,11 +1,12 @@
 import { rollback } from './migration';
-import { createClient } from '@clickhouse/client'
+import { createClient } from '@clickhouse/client-web'
 
 const clickhouse = createClient({
   host: Bun.env.CLICKHOUSE_HOST,
   username: Bun.env.CLICKHOUSE_USER,
   password: Bun.env.CLICKHOUSE_PASSWORD,
-  database: Bun.env.CLICKHOUSE_DATABASE
+  database: Bun.env.CLICKHOUSE_DATABASE,
+  keep_alive: { enabled: false },
 })
 
 rollback(clickhouse)
