613e66219f0b3d8cd8a4a355c83c3b1dfd8190ff
Andrei <soprachev@mail.ru>
2024-11-15 04:10:48 +0300

    update rerun script to use event-saver instead of prod

--- rerun-prod.sh
+++ rerun-prod.sh
@@ -1,7 +1,3 @@
-mkdir -p ../wot-src-data;
-chown -R 1000:1000 ../wot-src-data;
-rm ../wot-src-data/.git/index.lock;
-
-docker compose -p prod down;
-docker compose -p prod -f docker-compose.yaml -f docker-compose.prod.yaml pull;
-docker compose -p prod -f docker-compose.yaml -f docker-compose.prod.yaml up -d --remove-orphans;
+docker compose -p event-saver down;
+docker compose -p event-saver -f docker-compose.yaml -f docker-compose.prod.yaml pull;
+docker compose -p event-saver -f docker-compose.yaml -f docker-compose.prod.yaml up -d --remove-orphans;
