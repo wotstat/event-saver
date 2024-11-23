ccd24183243a793eb7fa805626bd9a61c394503f
Andrei <soprachev@mail.ru>
2024-11-23 20:36:16 +0300

    fix cicd

--- rerun-prod.sh
+++ rerun-prod.sh
@@ -1,3 +1,3 @@
 docker compose -p event-saver down;
 docker compose -p event-saver -f docker-compose.yaml pull;
-docker compose -p event-saver -f docker-compose.yaml up -d --remove-orphans;
+docker compose -p event-saver -f docker-compose.yaml up --build -d --remove-orphans;
